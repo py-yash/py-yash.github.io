04bd492074b8595337c4e212be46f9be01d570ca
py-yash <142984930+py-yash@users.noreply.github.com>
2023-08-23 16:40:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-
+   var typed = new Typed('#typed', {
+  strings: [
+    'Yash...',
+    'a self taught developer',
+    'a web Developer',
+    'a discord Bot developer',
+  ],
+  typeSpeed: 15,
+  
+  backSpeed: 15,
+  shuffle: true,
+  backDelay: 700,
+  loop: true,
+  loopCount: Infinity,
+  smartBackspace: true, // Default value
+});
